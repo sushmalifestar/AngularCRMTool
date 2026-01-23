db18f380e9885275dc967fe46213d0120b359112
Sai Sushma Cherukupalli <sushmalifestar@gmail.com>
2026-01-23 10:40:54 +0530

    binding concepts

--- src/app/componentPages/dashboard/dashboard.component.ts
+++ src/app/componentPages/dashboard/dashboard.component.ts
@@ -1,12 +1,22 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NgIf, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+ user:string ="Sushma"
+ isSeeLeadsClicked = false;
+ yourLeads : number =26;
+ customColor:string = "green";
+ searchText:string='';
 
+ onSeeLeadsClick(){
+  this.isSeeLeadsClicked = !this.isSeeLeadsClicked;
+ }
 }
